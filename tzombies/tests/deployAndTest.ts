e5de89498c17ef91edea1e6c5ee0abc8b5413082
grum-tez <grum.tez@gmail.com>
2023-07-09 05:22:02 +0000

    complete better testing

---
+++ tzombies/tests/deployAndTest.ts
@@ -0,0 +1,159 @@
+import { get_account, set_quiet } from '@completium/experiment-ts'
+import { Tzombies, add_for_all } from './bindings/tzombies'
+import { Permits } from './bindings/permits'
+import { Market } from './bindings/market'
+import { Nat, Bytes, Tez } from '@completium/archetype-ts-types'
+import { ledger_key } from './bindings/tzombies'
+import { expect_to_fail } from '@completium/experiment-ts'
+const assert = require('assert')
+
+// silence completium output:
+set_quiet(true)
+
+let permits: Permits = new Permits()
+let market: Market = new Market()
+let fa2: Tzombies = new Tzombies()
+
+const alice = get_account('alice')
+const bob = get_account('bob')
+
+describe('Contracts deployment', async () => {
+  it('permits', async () => {
+    await permits.deploy(alice.get_address(), { as: alice })
+    console.log('permits: ', permits.get_address())
+  })
+
+  it('market', async () => {
+    await market.deploy({ as: alice })
+    console.log('market: ', market.get_address())
+  })
+
+  it('fa2', async () => {
+    await fa2.deploy(alice.get_address(), permits.get_address(), { as: alice })
+    console.log('fa2: ', fa2.get_address())
+  })
+})
+
+describe('Register NFTs', async () => {
+  it('register zombie', async () => {
+    /* this calls the contract entrypoint:
+     * `entry set_token_metadata (tid : nat, tdata: map<string, bytes>)`
+     */
+    await fa2.set_token_metadata(
+      new Nat(1),
+      [
+        [
+          '',
+          new Bytes(
+            '697066733a2f2f516d53445733794257756e7977624c544c78723835784843464d6d747a5372365a55565138433375346161314d65'
+          ),
+        ],
+      ],
+      { as: alice }
+    )
+  })
+  it('register brainz', async () => {
+    await fa2.set_token_metadata(
+      new Nat(1),
+      [
+        [
+          '',
+          new Bytes(
+            '697066733a2f2f516d546d65517a55754b37716d467337795466563254434c5a416852466d716d714a793536636b6b7a666a586939'
+          ),
+        ],
+      ],
+      { as: alice }
+    )
+  })
+})
+
+describe('Mint and trade', async () => {
+  it('mint zombie', async () => {
+    /* calls the contract entrypoint:
+     * `entry mint (tow : address, tid : nat, nbt : nat)`
+     */
+    await fa2.mint(
+      alice.get_address(), // tow
+      new Nat(1), // tid
+      new Nat(1), // nbt
+      {
+        as: alice,
+        amount: new Tez(2),
+      }
+    )
+    /* check that Alice now has 1 zombie
+     * in order to check the balance, we need to read the contract storage defined by:
+     * `asset ledger identified by lowner ltokenid to big_map`
+     * in the following, we create the key we need to lookup (`lowner`, `ltokenid`)
+     * then we use the genreated `get_<asset_name>_value()` method
+     */
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount?.to_number() === 1)
+  })
+
+  it('sell zombie', async () => {
+    /* call the entrypoint:
+     * `entry list_for_sale(fa2_: address, token_id_: nat, amount_: nat, price_: tez, expiry_: date)`
+     */
+    await market.list_for_sale(
+      fa2.get_address(),
+      new Nat(1), // token_id_
+      new Nat(1), // amount_
+      new Tez(5), // price_
+      new Date('2023-12-01T00:00:00Z'), // expiry_
+      {
+        as: alice,
+      }
+    )
+  })
+
+  it('buy zombie before operator update should fail', async () => {
+    await expect_to_fail(async () => {
+      /* call the entrypoint:
+       * `entry buy(order_id: nat, amount_: nat)`
+       */
+      await market.buy(new Nat(1), new Nat(1), { amount: new Tez(5), as: bob })
+    }, fa2.errors.INVALID_CALLER)
+
+    // check that Alice still has 1 zombie
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount?.to_number() === 1)
+  })
+
+  it('approve marketplace', async () => {
+    /* the entrypoint
+     * `entry update_operators_for_all (upl : list<update_for_all_op>)`
+     * takes an enum value as parameter, the values are exposed as classes as well:
+     */
+    const arg = new add_for_all(market.get_address())
+    await fa2.update_operators_for_all([arg], { as: alice })
+  })
+
+  it('buy zombie without enough tez should fail', async () => {
+    await expect_to_fail(async () => {
+      await market.buy(new Nat(1), new Nat(1), { amount: new Tez(1), as: bob })
+    }, market.errors.r_value)
+
+    // check that Alice still has 1 zombie
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount?.to_number() === 1)
+  })
+
+  it('buy zombie', async () => {
+    await market.buy(new Nat(1), new Nat(1), { amount: new Tez(5), as: bob })
+
+    // check that Alice now has 0 zombie
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount === undefined)
+
+    // check that Bob now has 1 zombie
+    const key2 = new ledger_key(bob.get_address(), new Nat(1))
+    const amount2 = await fa2.get_ledger_value(key2)
+    assert(amount2?.to_number() === 1)
+  })
+})
